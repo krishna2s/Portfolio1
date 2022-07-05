c4179c89ea8cdd76f7d2674364fa1d109ace2651
krishna2s <krishnasarvaiya27@gmail.com>
2022-07-05 17:45:53 +0530

    first

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+
+function Header() {
+  return (
+
+    <>
+        <div className='header'>
+            <div className='h-left'>Header</div>
+            <div className='h-right'>Header r</div>
+        </div>
+    </>
+  )
+}
+
+export default Header
